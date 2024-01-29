cfe890d7ebe109d2b3a592b03ccb49327159e98e
verma7jr <amitkumarverma7840@gmail.com>
2024-01-29 13:16:19 +0530

    initial commit

---
+++ routes/task.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+import {newTask,getMyTask,deleteTask,updateTask} from "../controllers/task.js"
+import { isAuthenticated } from "../middlewares/auth.js";
+const router= express.Router();
+
+router.post("/new",isAuthenticated,newTask);
+router.get("/my",isAuthenticated,getMyTask);
+router.route("/:id").put(isAuthenticated,updateTask).delete(isAuthenticated,deleteTask);
+
+export default router;
